5fd3b286b260a92238d1beacfb7bff794921a03f
Jorge <celis244@gmail.com>
2021-04-12 23:10:15 -0400

    se quita / de app

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const multer = require("multer");
 const uuid = require("uuid");
 const path = require("path");
-const router = require("./routes"); 
+const router = require("./routes");
 
 const express = require("express");
 const app = express();
@@ -12,15 +12,15 @@ const app = express();
 app.set("view engine", "ejs");
 
 //middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 // const storage = multer.diskStorage({
 //     destination: path.join(__dirname, 'public/img/uploads'),
 //     filename: (req, file, cb, filename) => {
 //         console.log(file);
 //         cb(null, uuid() + path.extname(file.originalname));
 //     }
-// }) 
+// })
 // app.use(multer({storage}).single('image'));
 
 // Global variables
@@ -29,11 +29,8 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
 app.use(helmet());
 app.use(express.json());
-app.use("/", router);
+app.use("", router);
 
 (module.exports = app), morgan;
-
